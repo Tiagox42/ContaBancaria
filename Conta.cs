6fcf2eca3abc1e8f09c5efd15f4d59d2f506a1b6
Tiago Dantas <tiagodantas42@gmail.com>
2023-08-12 16:32:59 -0300

    Initial commit

---
+++ Conta.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaBancaria
+{
+    internal class Conta
+    {
+        private int _conta;
+        public string Nome { get; set; }
+        public double Saldo { get; private set; }
+
+        public Conta(int conta, string nome)
+        {
+            _conta = conta;
+            Nome = nome;
+        }
+        public Conta(int conta, string nome, double saldo) : this(conta, nome)
+        {
+
+            Deposito(saldo);
+        }
+
+        public void Deposito(double deposito)
+        {
+            Saldo += deposito;
+        }
+
+        public void Saque(double saque)
+        {
+            if (saque > 0.00)
+            {
+                Saldo = (Saldo - saque) - 5.00;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Conta: {_conta}, Titular: {Nome}, Saldo: R$" + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ContaBancaria;
+using System.Globalization;
+
+Conta operacao;
+
+Console.WriteLine("Bem vindo ao banco Dantas!");
+Console.WriteLine("Digite o número da conta:");
+int conta = int.Parse(Console.ReadLine());
+Console.WriteLine("Entre o titular da conta:");
+string nome = Console.ReadLine();
+Console.WriteLine("Haverá depósito inicial (s/n)?");
+char inicial = char.Parse(Console.ReadLine());
+double valorInicial = 0;
+if (inicial == 's' || inicial == 'S')
+{
+    Console.WriteLine("Entre o valor de depósito inicial:");
+    valorInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    operacao = new Conta(conta, nome, valorInicial);
+}
+else
+{
+    operacao = new Conta(conta, nome);
+}
+Console.WriteLine("Dados da conta:");
+Console.WriteLine(operacao);
+
+Console.WriteLine("Entre um valor para depósito:");
+double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+operacao.Deposito(deposito);
+Console.WriteLine("Dados da conta atualizados:");
+Console.WriteLine(operacao);
+
+Console.WriteLine("Entre um valor para saque:");
+double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+operacao.Saque(saque);
+Console.WriteLine("Dados da conta atualizados:");
+Console.WriteLine(operacao);
+
+Console.WriteLine("-----------------\nDeseja alterar o nome de sua conta? (s/n)?");
+char opcaoNome = char.Parse(Console.ReadLine());
+if (opcaoNome == 's' || opcaoNome == 'S')
+{
+    Console.WriteLine("Digite o novo nome da conta:");
+    string novoNome = Console.ReadLine();
+    operacao.Nome = novoNome;
+    Console.WriteLine("Dados da conta atualizados:");
+    Console.WriteLine(operacao);
+}
